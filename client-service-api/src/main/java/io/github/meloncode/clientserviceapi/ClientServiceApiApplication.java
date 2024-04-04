90f8053d8d6e6298de5e299fe68dd614ed1271cc
aleffchaves <alef.chaves01@gmail.com>
2024-04-04 07:58:57 -0300

    Adding eureka client on client-service-api

---
+++ client-service-api/src/main/java/io/github/meloncode/clientserviceapi/ClientServiceApiApplication.java
@@ -0,0 +1,12 @@
+package io.github.meloncode.clientserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientServiceApiApplication.class, args);
+	}
+}
---
+++ client-service-api/src/test/java/io/github/meloncode/clientserviceapi/ClientServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.meloncode.clientserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
